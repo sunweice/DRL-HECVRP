7a8a5acd860e3a12fb13bda903b1d0fadb732891
Weice <47023881+sunweice@users.noreply.github.com>
2026-02-27 10:15:26 +0800

    Add files via upload

---
+++ reproducibility/comparetime.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+plt.rcParams["font.family"] = ["Times New Roman", "SimSun"]
+df = pd.read_csv('runtime.csv')
+
+
+configs = df['config'].tolist()
+mha_times = df['mha_time'].tolist()
+ge_mha_times = df['ge_mha_time'].tolist()
+# configs = ['C10-RC3-V3', 'C10-RC3-V5', 'C20-RC3-V3', 'C20-RC3-V5', 'C50-RC6-V3', 'C50-RC6-V5']
+# mha_times = [2.53, 3.05, 5.22, 6.33, 11.08, 11.25]
+# ge_mha_times = [2.77, 3.61, 5.55, 6.38, 11.25, 13.64]
+
+# 创建折线图
+plt.figure(figsize=(8, 5))
+plt.plot(configs, mha_times, label='MHA', color='orange', marker='o', linewidth=2, alpha=0.7)
+plt.plot(configs, ge_mha_times, label='GE-MHA', color='red', marker='s', linewidth=2, alpha=0.7)
+
+# 图形设置
+plt.xlabel('配置',fontsize=20)
+plt.ylabel('训练时间 (h)',fontsize=20)
+plt.xticks(fontsize=13)
+plt.yticks(fontsize=13)
+plt.legend(fontsize=20)
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('trainingtime.png', dpi=300)
+# 显示图像
+plt.show()
