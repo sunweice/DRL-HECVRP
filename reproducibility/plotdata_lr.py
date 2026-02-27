e09f82c7f2d0a6d9e21a4e61a67f9099eaae9d82
Weice <47023881+sunweice@users.noreply.github.com>
2026-02-27 09:56:34 +0800

    Add files via upload

---
+++ reproducibility/plotdata_lr.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 设置字体为 Times New Roman
+plt.rcParams["font.family"] = ["Times New Roman", "SimSun"]
+
+def plot(df1,df2,df3):
+    # 平滑函数：移动平均
+    def smooth(data, window_size=10):
+        return data.rolling(window=window_size, min_periods=1).mean()
+
+
+    # 平滑数据
+    df1['Value_smooth'] = smooth(df1['Value'], window_size=15)
+    df2['Value_smooth'] = smooth(df2['Value'], window_size=15)
+    df3['Value_smooth'] = smooth(df3['Value'], window_size=15)
+
+    # 假设两张表都有 'step' 和 'value' 两列
+    plt.figure(figsize=(10, 6))
+    # Curve 1：原始 + 平滑
+    plt.plot(df1['Step'], -df1['Value'], color='red', alpha=0.4, linewidth=2)
+    plt.plot(df1['Step'], -df1['Value_smooth'], label=r'lr=$10^{-3}$',color='red', linewidth=2)
+
+    # Curve 2：原始 + 平滑
+    plt.plot(df2['Step'], -df2['Value'], color='green', alpha=0.4, linewidth=2)
+    plt.plot(df2['Step'], -df2['Value_smooth'],label=r'lr=$10^{-4}$', color='green', linewidth=2)
+
+    # Curve 2：原始 + 平滑
+    plt.plot(df2['Step'], -df3['Value'], color='orange', alpha=0.4, linewidth=2)
+    plt.plot(df2['Step'], -df3['Value_smooth'],label='lr=$10^{-5}$', color='orange', linewidth=2)
+
+    plt.ylim(-1.5,-4)
+    # 美化图表
+    plt.xlabel('Step',fontsize=20)
+    plt.ylabel('Reward',fontsize=20)
+    # plt.title('Comparison of Two Curves')
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.legend(fontsize=20)
+    plt.grid(True)
+
+    # 保存或显示
+    plt.savefig('lr.png', dpi=300)
+    plt.show()
+
+# 读取两个 CSV 文件
+df1 = [pd.read_csv('hecvrp_rollout_rl03.csv')]  # 替换为你的文件路径
+df2 = [pd.read_csv('hecvrp_rollout_rl04.csv')]
+df3 = [pd.read_csv('hecvrp_rollout_rl05.csv')]
+for d1,d2,d3 in zip(df1,df2,df3):
+    plot(d1,d2,d3)
